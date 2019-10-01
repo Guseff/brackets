c547f7cbf76d9cef7b5879fb207cfb2ca141b823
Vasily Gusev <vasily.guseff@gmail.com>
2019-10-01 12:09:13 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const specials = '[]{}\^$.|?*+()';
+  const brackets = bracketsConfig.map(curr => {
+    if (specials.includes(curr[0])) {
+      return '\\' + curr[0] + '\\' + curr[1];
+    } else {
+      return curr[0] + curr[1];
+    }
+  }).join('|');
+  const reg = new RegExp(brackets);
+  let string = str;
+
+  while (true) {
+    if (reg.test(string)) {
+      string = string.replace(reg, '');
+      continue;
+    }
+    break;
+  }
+
+  return string.length === 0 ? true : false;
 }
